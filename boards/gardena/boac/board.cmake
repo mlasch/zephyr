10b761c2f1725382aecdab168204241f9cdcdd80
Marc Lasch <marc.lasch@husqvarnagroup.com>
2025-11-29 19:56:26 +0100

    fixup! boards: gardena: Add AquaPrecise

--- boards/gardena/boac/board.cmake
+++ boards/gardena/boac/board.cmake
@@ -1,6 +1,7 @@
 # Copyright (c) 2025 Reto Schneider
+# Copyright (c) 2025 Marc Lasch
 #
 # SPDX-License-Identifier: Apache-2.0
 
 board_runner_args(jlink "--device=nRF52840_xxAA" "--speed=4000")
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
